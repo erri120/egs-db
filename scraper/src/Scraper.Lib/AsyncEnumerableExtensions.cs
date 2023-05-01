5274963816c0cc19fffdd479247110f776f21b68
erri120 <erri120@protonmail.com>
2023-05-01 14:31:37 +0200

    Add ToListSafeAsync

--- scraper/src/Scraper.Lib/AsyncEnumerableExtensions.cs
+++ scraper/src/Scraper.Lib/AsyncEnumerableExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ public static class AsyncEnumerableExtensions
     /// <param name="cancellationToken"></param>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
+    /// <seealso cref="ToListSafeAsync{T}"/>
     public static async Task<IList<T>> ToListAsync<T>(
         [InstantHandle(RequireAwait = true)] this IAsyncEnumerable<T> asyncEnumerable,
         CancellationToken cancellationToken = default)
@@ -28,6 +30,38 @@ public static class AsyncEnumerableExtensions
         await foreach (var item in asyncEnumerable.WithCancellation(cancellationToken).ConfigureAwait(false))
         {
             list.Add(item);
+        }
+
+        return list;
+    }
+
+    /// <summary>
+    /// Asynchronously enumerates the entire <see cref="IAsyncEnumerable{T}"/>
+    /// and puts the elements into a <see cref="IList{T}"/>. This function
+    /// safely enumerates and returns the list when an exception occurs, to not
+    /// loose any data.
+    /// </summary>
+    /// <param name="asyncEnumerable"></param>
+    /// <param name="cancellationToken"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    /// <seealso cref="ToListAsync{T}"/>
+    public static async Task<IList<T>> ToListSafeAsync<T>(
+        [InstantHandle(RequireAwait = true)] this IAsyncEnumerable<T> asyncEnumerable,
+        CancellationToken cancellationToken = default)
+    {
+        var list = new List<T>();
+
+        try
+        {
+            await foreach (var item in asyncEnumerable.WithCancellation(cancellationToken).ConfigureAwait(false))
+            {
+                list.Add(item);
+            }
+        }
+        catch (Exception)
+        {
+            return list;
         }
 
         return list;
